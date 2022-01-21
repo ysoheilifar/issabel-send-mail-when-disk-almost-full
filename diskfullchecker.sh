745848a422085de9755ff0ab9bc44cab78ca0714
Yaser Soheilifar <83553075+ysoheilifar@users.noreply.github.com>
2022-01-21 18:33:01 +0330

    Update diskfullchecker.sh

--- diskfullchecker.sh
+++ diskfullchecker.sh
@@ -1,7 +1,7 @@
  #!/bin/bash  
  CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')  
  THRESHOLD=90
- TO=y.soheilifar@gmail.com  
+ TO=issabel_Gmail_account@gmail.com 
    
  if [ "$CURRENT" -gt "$THRESHOLD" ] ; then  
    ./phpmailer.php $TO "Email from issabel" "Disk is Full" 
